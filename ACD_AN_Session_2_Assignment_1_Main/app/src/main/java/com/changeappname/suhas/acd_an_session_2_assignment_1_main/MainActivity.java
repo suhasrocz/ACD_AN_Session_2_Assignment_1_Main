de5f28c3029c37315bf6562f1f005b5779e4c6fa
suhasrocz <suhasrocz@gmail.com>
2016-09-16 16:25:06 +0530

    ACD_AN_Session_2_Assignment_1_Main
    app name changed and icon launcher changed

---
+++ ACD_AN_Session_2_Assignment_1_Main/app/src/main/java/com/changeappname/suhas/acd_an_session_2_assignment_1_main/MainActivity.java
@@ -0,0 +1,19 @@
+package com.changeappname.suhas.acd_an_session_2_assignment_1_main;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.mipmap.new_icon);
+
+    }
+}
